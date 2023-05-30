d85655efdd04cf4112997497181ca339f2c6858e
lesaprodan <99794388+lesaprodan@users.noreply.github.com>
2023-05-30 22:04:00 +0300

    Update OOPLab6T.cpp

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -77,3 +77,103 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+#include <string>
+
+class Transport {
+protected:
+    std::string name;
+public:
+    Transport(const std::string& transportName) : name(transportName) {}
+    virtual ~Transport() {}
+
+    virtual void displayInfo() const {
+        std::cout << "Transport: " << name << std::endl;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Transport& transport) {
+        os << "Transport: " << transport.name;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Transport& transport) {
+        is >> transport.name;
+        return is;
+    }
+};
+
+class Ship : virtual public Transport {
+protected:
+    int shipId;
+public:
+    Ship(const std::string& shipName, int id) : Transport(shipName), shipId(id) {}
+    virtual ~Ship() {}
+
+    virtual void displayInfo() const override {
+        std::cout << "Ship Name: " << name << ", ID: " << shipId << std::endl;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Ship& ship) {
+        os << static_cast<const Transport&>(ship) << ", ID: " << ship.shipId;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Ship& ship) {
+        is >> static_cast<Transport&>(ship) >> ship.shipId;
+        return is;
+    }
+};
+
+class PassengerTransport : virtual public Transport {
+protected:
+    int passengerCapacity;
+public:
+    PassengerTransport(const std::string& transportName, int capacity) : Transport(transportName), passengerCapacity(capacity) {}
+    virtual ~PassengerTransport() {}
+
+    virtual void displayInfo() const override {
+        std::cout << "Passenger Transport: " << name << ", Capacity: " << passengerCapacity << std::endl;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const PassengerTransport& transport) {
+        os << static_cast<const Transport&>(transport) << ", Capacity: " << transport.passengerCapacity;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, PassengerTransport& transport) {
+        is >> static_cast<Transport&>(transport) >> transport.passengerCapacity;
+        return is;
+    }
+};
+
+class PassengerShip : public Ship, public PassengerTransport {
+public:
+    PassengerShip(const std::string& shipName, int id, int capacity)
+        : Transport(shipName), Ship(shipName, id), PassengerTransport(shipName, capacity) {}
+
+    virtual void displayInfo() const override {
+        std::cout << "Passenger Ship Name: " << name << ", ID: " << shipId << ", Capacity: " << passengerCapacity << std::endl;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const PassengerShip& ship) {
+        os << static_cast<const Ship&>(ship) << ", Capacity: " << ship.passengerCapacity;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, PassengerShip& ship) {
+        is >> static_cast<Transport&>(ship) >> static_cast<Ship&>(ship) >> ship.passengerCapacity;
+        return is;
+    }
+};
+
+int main() {
+    PassengerShip ship("Titanic", 123, 2000);
+    ship.displayInfo();
+
+    std::cout << "Enter details for a passenger ship: ";
+    std::cin >> ship;
+    std::cout << ship << std::endl;
+
+    return 0;
+}
